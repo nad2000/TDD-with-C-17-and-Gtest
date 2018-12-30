2466aa7de8399af38975f8ffdbda247bdf8b8216
Rad Cirskis <nad2000@gmail.com>
2018-12-31 00:34:55 +0900

    first 3 requirements ready

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -2,11 +2,22 @@
 #include <gtest/gtest.h>
 
 std::string fizzBuzz(int v) {
-	return "";
+	if (v == 1) return "1";
+	else if (v == 2) return "2";
 }
 
 TEST(FizzBuzzTest, canCallFizzBuzz) {
 	auto result = fizzBuzz(1);
+}
+
+TEST(FizzBuzzTest, returns1With1PassedIn) {
+	auto result = fizzBuzz(1);
+	ASSERT_STREQ(result.c_str(), "1");
+}
+
+TEST(FizzBuzzTest, returns2With2PassedIn) {
+	auto result = fizzBuzz(2);
+	ASSERT_STREQ(result.c_str(), "2");
 }
 
 int main(int argc, char **argv) {
